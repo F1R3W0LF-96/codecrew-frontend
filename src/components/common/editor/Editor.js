92ff5d885b8c4207571cc78211a28749c1020c81
Ankit-digimantra <ankit.parashar@digimantra.com>
2022-08-14 02:47:13 +0530

    "minor changes for editor "

--- src/components/common/editor/Editor.js
+++ src/components/common/editor/Editor.js
@@ -5,28 +5,63 @@ import { EditorState, convertToRaw } from "draft-js";
 import draftToHtml from "draftjs-to-html";
 import "./Editor.css";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { Button } from "react-bootstrap";
+import axios from "axios";
 
 
 function ContentEditor() {
-    const [description, setDescription] = useState({
-        htmlValue: "<p></p>\n",
-        editorState: EditorState.createEmpty()
-      });
-    
-      const onEditorStateChange = editorValue => {
-        const editorStateInHtml = draftToHtml(
-          convertToRaw(editorValue.getCurrentContent())
-        );
-    
-        setDescription({
-          htmlValue: editorStateInHtml,
-          editorState: editorValue
-        });
-      };
+  const [description, setDescription] = useState({
+    htmlValue: "<p></p>\n",
+    editorState: EditorState.createEmpty()
+  });
+  const [title, setTitle] = useState("");
+  const [artdescription, setArtDescription] = useState("");
+const [topic, setTopic] = useState("");
+const [subtopic, setSubtopic] = useState("");
+
+
+
+  const onEditorStateChange = editorValue => {
+    const editorStateInHtml = draftToHtml(
+      convertToRaw(editorValue.getCurrentContent())
+    );
+
+    setDescription({
+      htmlValue: editorStateInHtml,
+      editorState: editorValue
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    debugger
+    console.log(description.htmlValue);
+    const payload = {
+      "title": title,
+      "topic": topic,
+      "subtopic": subtopic,
+      "description": artdescription,
+      "body": description.htmlValue,
+      "createdBy": "62f772ee48774d641e2cb2e4"
+    }
+    axios.post('https://ad08-2409-4056-99-ed86-526-be8c-50b8-9cb1.ngrok.io/api/article', payload).then(res => {
+      console.log(res);
+    }
+    ).catch(err => {
+      console.log(err);
+    }
+    );
+  }
   return (
 
     <div>
-        <Editor
+      <input type="text" name="title" placeholder="Title" onChange={(e)=>setTitle(e.target.value)} />
+      <input type="text" name="topic" placeholder="Topic" onChange={(e)=>setTopic(e.target.value)} />
+      <input type="text" name="subtopic" placeholder="Subtopic" onChange={(e)=>setSubtopic(e.target.value)} />
+      <input type="text" name="description" placeholder="Description" onChange={(e)=>setArtDescription(e.target.value)} />
+
+
+      <Editor
         toolbarHidden={false}
         editorState={description.editorState}
         onEditorStateChange={onEditorStateChange}
@@ -35,13 +70,14 @@ function ContentEditor() {
       <textarea
         disabled
         value={description.htmlValue}
-      
+
       >
       </textarea>
-     <button onClick={() => {
-        console.log(description.htmlValue);
-      } }>click me 
-      </button>
+      <br />
+      <Button onClick={(e) => {
+        handleSubmit(e);
+      }}>Submit Article
+      </Button>
     </div>
   )
 }
